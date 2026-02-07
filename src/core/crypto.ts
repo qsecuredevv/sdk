5b458cdc99652637ce3dfadecbda76b0657420ab
Qsecure <zekychen30@gmail.com>
2026-02-07 08:29:25 +0700

    Create crypto.ts

---
+++ src/core/crypto.ts
@@ -0,0 +1,8 @@
+import crypto from "crypto";
+
+export function generateKeyPair() {
+  return crypto.generateKeyPairSync("ed25519", {
+    publicKeyEncoding: { type: "spki", format: "pem" },
+    privateKeyEncoding: { type: "pkcs8", format: "pem" }
+  });
+}
